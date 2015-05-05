0b91c4094785a7e72b39839745139ed958604880
Stephen Mckellar <stephen@thingmaker.io>
2015-05-05 12:35:27 +0100

    Give the correct url in the URL generator!

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+  });
+}
+
 $(function() {
   $('#forecast').submit(function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var forecastLocation = $('#forecast_location').val();
-    $('#calendar_url').val('https://' + location.host + '/' + forecastLocation + '/forecast.ics');
+    var forecastLocation = toTitleCase($('#forecast_location').val());
+    $('#calendar_url').val('https://' + location.host + '/forecast/' + forecastLocation + '.ics');
     $('#calendar').show();
   });
 });
