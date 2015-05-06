71cd86343dd98c54175eb0e5b8622c03ca2e607d
Stephen Mckellar <stephen@thingmaker.io>
2015-05-06 16:29:23 +0100

    Bug fix for todays weather preview

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -17,12 +17,15 @@ $(function() {
 
     var $forecastPreview = $('#forecast_preview');
     $.get(calendar_address + '.json', function(data) {
+      $forecastPreview.empty();
       var today = moment();
+      var foundRain = false;
       for(var i = 0; i < data.length; i++) {
         var appointment = data[i];
         var timeFrom = moment(appointment['time_from']);
         var timeTo = moment(appointment['time_to']);
-        if(true || timeFrom.isSame(today, 'day')) {
+        if(timeFrom.isSame(today, 'day')) {
+          foundRain = true;
           var times = timeFrom.format('HH:mm') + ' - ' + timeTo.format('HH:mm');
           var $appointment = $('<div class="card blue darken-2"><div class="card-content white-text"><div class="card-title"></div><div class="appointment-times"></div></div></div>');
           $appointment.find('.card-title').text('Rain');
@@ -30,7 +33,10 @@ $(function() {
           $forecastPreview.append($appointment);
         }
       }
-      $forecastPreview.show();
+      if(!foundRain) {
+        $forecastPreview.append('<p>No rain today!</p>');
+      }
+      $('#forecast_for_today').show();
     });
   });
 });
