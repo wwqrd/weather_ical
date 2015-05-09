bd62499229146db550302ff5bec9b82e93c79683
Stephen Mckellar <stephen@thingmaker.io>
2015-05-09 11:38:09 +0100

    Fixed ForecastController spec

--- spec/controllers/forecast_controller_spec.rb
+++ spec/controllers/forecast_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ForecastsController, type: :controller do
+describe ForecastController, type: :controller, vcr: true do
+
+  let(:location) { 'London' }
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, { :format => :json, :location => location }
       expect(response).to have_http_status(:success)
     end
   end
