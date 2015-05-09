3a673de418ef559b52be4f06e9f3d9bc1b8b9de2
Stephen Mckellar <stephen@thingmaker.io>
2015-05-09 10:52:52 +0100

    Fixed ForecastCalendar spec

--- spec/models/forecast_calendar_spec.rb
+++ spec/models/forecast_calendar_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 describe ForecastCalendar, :vcr => true do
 
+  let(:event_name) { 'Rain' }
   let(:location) { "London" }
   let(:weather_service) { WeatherService.new(location) }
   let(:forecast_series) { weather_service.hour_by_hour }
+  let(:rain_filter) { proc { |forecast| forecast.precipitation > 0.0 } }
+  let(:rain_calendar) do
+    ForecastCalendar.Generate(forecast_series, event_name, &rain_filter)
+  end
 
   describe '#ics' do
-    let(:rain_calendar) { ForecastCalendar.Rain(forecast_series) }
     subject(:ics) { rain_calendar.to_ics }
 
     it 'returns the forecast in .ics format' do
@@ -18,10 +22,7 @@ describe ForecastCalendar, :vcr => true do
 
   end
 
-  describe '.Rain' do
-
-    subject(:rain_calendar) { ForecastCalendar.Rain(forecast_series) }
-
+  describe '.Generate' do
     it 'creates a ForecastCalendar' do
       expect(rain_calendar).to be_a(ForecastCalendar)
     end
